263753cb1743c29ca97cc44ae7cf7c3b7f00bbf1
Joona Yoon <joonas.yoon@gmail.com>
2022-09-13 20:44:50 +0900

    feat(login): create form component to login

---
+++ react-app/src/components/LoginForm.jsx
@@ -0,0 +1,212 @@
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  Typography,
+  styled,
+} from '@mui/material';
+import { IoEye, IoEyeOff } from 'react-icons/io5';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { FaCheck } from 'react-icons/fa';
+
+export const LoginContainer = styled(Container)`
+  width: 40ch;
+  margin-top: 2em;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1.5em;
+  box-shadow: 0px 2px 2px -1px rgb(0 0 0 / 20%), 0px 1px 2px 0px rgb(0 0 0 / 12%),
+    0px 3px 5px 0px rgb(0 0 0 / 10%);
+`;
+
+const validate = ({ username, password }) => {
+  if (username.length < 1) {
+    return 'Username is required.';
+  }
+  if (password.length < 1) {
+    return 'Password is required.';
+  }
+  return null;
+};
+
+export const Title = styled(Typography)`
+  font-weight: bold;
+  font-size: 1.5em;
+`;
+
+export const SubTitle = styled(Typography)`
+  font-size: 0.8rem;
+`;
+
+const Input = (props) => {
+  const { error } = props;
+  const style = useMemo(() => {
+    return error
+      ? {
+          border: '1px solid #d32f2f',
+          borderRadius: '5px',
+        }
+      : {};
+  }, [error]);
+  return (
+    <FormControl variant="outlined" error={error || false} sx={style}>
+      {props.children}
+    </FormControl>
+  );
+};
+
+const LoginButton = (props) => {
+  const { status, children, ...others } = props;
+
+  const isLoading = status === 'waiting';
+  const isSuccess = status === 'success';
+
+  const iconSize = {
+    width: '1.5em !important',
+    height: '1.5em !important',
+  };
+
+  const injectedProps = {
+    ...others,
+    sx: {
+      height: '3em',
+    },
+  };
+
+  const Result = useCallback(() => {
+    if (isSuccess) {
+      return (
+        <Button color="success" disableElevation {...injectedProps}>
+          <FaCheck color="white" sx={iconSize} />
+        </Button>
+      );
+    } else if (isLoading) {
+      return (
+        <Button color="primary" loading="true" disabled {...injectedProps}>
+          <CircularProgress color="primary" sx={iconSize} />
+        </Button>
+      );
+    } else {
+      return (
+        <Button color="primary" {...injectedProps}>
+          {children}
+        </Button>
+      );
+    }
+  }, [status]);
+
+  return <Result />;
+};
+
+const LoginForm = (props) => {
+  const { onSubmit, serverResponse } = props;
+
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    showPassword: false,
+    errorMessage: null,
+  });
+
+  const setValue = (key, value) => {
+    setValues({ ...values, [key]: value });
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({
+      ...values,
+      errorMessage: null,
+      [prop]: event.target.value,
+    });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues((prevValue) => ({
+      ...values,
+      showPassword: !prevValue.showPassword,
+    }));
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const hasError = useMemo(() => {
+    return !!values.errorMessage;
+  }, [values.errorMessage]);
+
+  useEffect(() => {
+    setValue('errorMessage', serverResponse.status === 'error' ? serverResponse.message : null);
+    console.log(serverResponse);
+  }, [serverResponse]);
+
+  return (
+    <Stack
+      component="form"
+      spacing={2}
+      noValidate
+      autoComplete="off"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        const err = validate(values);
+        const isValid = err === null;
+        setValue('errorMessage', err);
+        if (isValid) {
+          onSubmit(values.username, values.password);
+        }
+        return isValid;
+      }}
+    >
+      <Input error={hasError}>
+        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-username"
+          type="text"
+          value={values.username}
+          onChange={handleChange('username')}
+          label="Username"
+        />
+      </Input>
+      <Input error={hasError}>
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {values.showPassword ? <IoEye /> : <IoEyeOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </Input>
+      {hasError && <Alert severity="error">{values.errorMessage}</Alert>}
+      <LoginButton
+        type="submit"
+        variant="contained"
+        status={serverResponse ? serverResponse.status : undefined}
+      >
+        Login
+      </LoginButton>
+    </Stack>
+  );
+};
+
+export default LoginForm;
--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
-import { Container } from '@mui/material';
+import LoginForm, { LoginContainer, SubTitle, Title } from 'components/LoginForm';
+import React, { useState } from 'react';
+
+import { Box } from '@mui/material';
+import { customAxios } from 'libs/customAxios';
+import qs from 'qs';
 
 const Login = () => {
+  const [serverResponse, setServerResponse] = useState({
+    status: null,
+    message: '',
+  });
+
+  const onSubmit = (username, password) => {
+    const formData = qs.stringify({
+      username,
+      password,
+    });
+
+    setServerResponse({
+      status: 'waiting',
+    });
+
+    customAxios
+      .post('/auth/jwt/login', formData, {
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      })
+      .then((response) => {
+        console.log('Response', response);
+        const { accessToken } = response.data;
+        localStorage.setItem('access_token', JSON.stringify(accessToken || {}));
+        setServerResponse({
+          status: 'success',
+          message: null,
+        });
+      })
+      .catch(({ message }) => {
+        console.error(`Failed to request : ${message}`);
+        const ERROR_MESSAGE = 'Your login attempt was not successful. Try again';
+        setServerResponse({
+          status: 'error',
+          message: ERROR_MESSAGE,
+        });
+      });
+  };
+
   return (
-    <Container>
-      <h1>LoginPage</h1>
-    </Container>
+    <LoginContainer>
+      <Box
+        sx={{
+          marginBottom: '1em',
+        }}
+      >
+        <Title>Login</Title>
+        <SubTitle>Welcome back! Sign in to continue</SubTitle>
+      </Box>
+      <LoginForm onSubmit={onSubmit} serverResponse={serverResponse} />
+    </LoginContainer>
   );
 };
 
