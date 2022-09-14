b635787728007ede59c2af44ba51daa0a794a121
Joona Yoon <joonas.yoon@gmail.com>
2022-09-15 02:01:12 +0900

    fix(storage): set and get simple

--- react-app/src/libs/storage.ts
+++ react-app/src/libs/storage.ts
@@ -1,34 +1,18 @@
-const STORAGE_NAME = 'storage';
-
 class Storage {
   cache: Map<string, any>;
 
   constructor() {
-    this.cache = this.unpack();
-    console.log('Storage:', this.cache);
-  }
-
-  unpack(): Map<string, any> {
-    try {
-      const data = window.localStorage.getItem(STORAGE_NAME) as string;
-      return new Map<string, any>(Object.entries(JSON.parse(data)));
-    } catch (err) {
-      return new Map<string, any>();
-    }
-  }
-
-  pack(): string {
-    return JSON.stringify(Object.fromEntries(this.cache));
+    this.cache = new Map<string, any>();
   }
 
   set(key: string, value: any): void {
     this.cache.set(key, value);
-    window.localStorage.setItem(STORAGE_NAME, this.pack());
+    window.localStorage.setItem(key, value);
   }
 
   get(key: string, initialValue: any): any {
     try {
-      return this.cache.get(key) || initialValue;
+      return this.cache.get(key) || window.localStorage.getItem(key) || initialValue;
     } catch (err) {
       return initialValue;
     }
