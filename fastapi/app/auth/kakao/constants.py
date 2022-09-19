ad7f7f88cbd889ed2fd6e9c7d695e390b8cbf9fb
Joona Yoon <joonas.yoon@gmail.com>
2022-09-19 23:29:36 +0900

    feat(auth.kakao): implements update user profile

---
+++ fastapi/app/auth/kakao/constants.py
@@ -0,0 +1,1 @@
+KAKAO_USERINFO_URL = 'https://kapi.kakao.com/v2/user/me'
--- fastapi/app/auth/kakao/libs.py
+++ fastapi/app/auth/kakao/libs.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 from typing import Any, Optional
 
 import requests
@@ -9,19 +10,43 @@ from fastapi_users.authentication.strategy import Strategy
 from ..exceptions import BadCredentialException
 from ..libs import bearer_transport, get_jwt_strategy
 from ..models import User
+from .constants import KAKAO_USERINFO_URL
 
 
 class KakaoAuthBackend(AuthenticationBackend):
     async def login(self, strategy: Strategy, user: User, response: Response) -> Any:
         strategy_response = await super().login(strategy, user, response)
-        print('user', user)
+        token = self.get_access_token(user)
+        profile = get_profile(token)
+        await update_profile(user, profile).save()
         return strategy_response
 
-    def get_google_access_token(self, user: User) -> Optional[str]:
+    def get_access_token(self, user: User) -> Optional[str]:
         for account in user.oauth_accounts:
             if account.oauth_name == 'kakao':
                 return account.access_token
         return None
+
+
+def get_profile(access_token: str) -> dict:
+    headers = dict(Authorization=f'Bearer {access_token}')
+    response = requests.post(KAKAO_USERINFO_URL,
+                             headers=headers,
+                             params={"property_keys": json.dumps(["kakao_account.profile"])})
+    if not response.ok:
+        raise BadCredentialException(
+            'Failed to get user information from Kakao.')
+
+    profile = dict(response.json())
+    kakao_account = profile.get('kakao_account')
+    return kakao_account.get('profile')
+
+
+def update_profile(user: User, profile: dict) -> User:
+    if user.picture == None:
+        user.picture = profile.get('profile_image_url')
+    user.last_login_at = datetime.now()
+    return user
 
 
 auth_backend_kakao = KakaoAuthBackend(
